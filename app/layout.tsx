373f8d868d9e80f01f73005fd821f9b48388f8f8
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-03-08 22:42:14 -0500

    feat: footer and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import PrelineScript from "@/components/PrelineScript";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
     <html lang="en" className="light">
       <body className={inter.className}>
         <Header />
-        {children}</body>
+        {children}
+        <Footer />
+      </body>
       <PrelineScript />
     </html>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+import { IoMenu, IoClose } from "react-icons/io5";
+
 export default function Header() {
   return (
     <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-4 dark:bg-gray-800">
@@ -19,37 +21,10 @@ export default function Header() {
             aria-controls="navbar-alignment"
             aria-label="Toggle navigation"
           >
-            <svg
-              className="hs-collapse-open:hidden flex-shrink-0 size-4"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <line x1="3" x2="21" y1="6" y2="6" />
-              <line x1="3" x2="21" y1="12" y2="12" />
-              <line x1="3" x2="21" y1="18" y2="18" />
-            </svg>
-            <svg
-              className="hs-collapse-open:block hidden flex-shrink-0 size-4"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path d="M18 6 6 18" />
-              <path d="m6 6 12 12" />
-            </svg>
+            
+                      <IoMenu className="hs-collapse-open:hidden flex-shrink-0 size-4" />
+                        <IoClose className="hs-collapse-open:block hidden flex-shrink-0 size-4" />
+            
           </button>
           <button
             type="button"
